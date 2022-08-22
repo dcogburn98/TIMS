3c4231d1bfcf46696992a9a9392e401e98a9340c
Blake Cogburn <daniel.blakecogburn@gmail.com>
2022-08-22 13:00:54 -0500

    Changes to android app

---
+++ TIMSMobile/Communication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using TIMSServerModel;
+
+namespace TIMSMobile
+{
+    class Communication
+    {
+        private static ChannelFactory<ITIMSServiceModel> channelFactory = new
+            ChannelFactory<ITIMSServiceModel>("TIMSServerEndpoint");
+
+        private static ITIMSServiceModel proxy = channelFactory.CreateChannel();
+
+        public static List<Checkin> RetrieveCheckins()
+        {
+            return proxy.RetrieveCheckins();
+        }
+    }
+}
--- TIMSMobile/MainActivity.cs
+++ TIMSMobile/MainActivity.cs
@@ -1,10 +1,14 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+
+using Android.App;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+
+using TIMSServerModel;
 
 namespace TIMSMobile
 {
@@ -36,15 +40,26 @@ namespace TIMSMobile
                 case Resource.Id.navigation_home:
                     textMessage.SetText(Resource.String.title_home);
                     return true;
-                case Resource.Id.navigation_dashboard:
-                    textMessage.SetText(Resource.String.title_dashboard);
-                    return true;
                 case Resource.Id.navigation_notifications:
                     textMessage.SetText(Resource.String.title_notifications);
                     return true;
                 case Resource.Id.navigation_settings:
                     textMessage.SetText(Resource.String.title_settings);
                     return true;
+                case Resource.Id.navigation_checkins:
+                    {
+                        Spinner checkinSpinner = FindViewById<Spinner>(Resource.Id.selectcheckin);
+                        List<Checkin> checkins = Communication.RetrieveCheckins();
+                        string[] checkinNumbers = new string[checkins.Count];
+                        for (int i = 0; i != checkins.Count; i++)
+                            checkinNumbers[i] = checkins[i].checkinNumber.ToString();
+
+                        ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerItem, checkinNumbers);
+                        checkinSpinner.Adapter = adapter;
+                        textMessage.SetText(Resource.String.title_checkins);
+                        SetContentView(Resource.Layout.checkin_main);
+                        return true;
+                    }
             }
             return false;
         }
